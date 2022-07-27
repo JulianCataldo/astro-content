5f07d7497f64474fcf514fce0d04f49f0a72a8fd
Julian Cataldo <contact@juliancataldo.com>
2022-07-27 22:19:17 +0200

    chore: add some tooling for demo website

---
+++ demo/.eslintrc.cjs
@@ -0,0 +1,7 @@
+// /** @type {import("@types/eslint").Linter.Config} */
+
+// module.exports = {
+//   // FIX: ./node_modules/ prefix is required, IDK why
+//   extends: ['./node_modules/@julian_cataldo/astro-configs/.eslintrc.cjs'],
+// };
+ 
---
+++ demo/.prettierrc.cjs
@@ -0,0 +1,5 @@
+/** @type {import("@types/prettier").Options} */
+
+module.exports = {
+  ...require("@julian_cataldo/astro-configs/.prettierrc.cjs"),
+};
