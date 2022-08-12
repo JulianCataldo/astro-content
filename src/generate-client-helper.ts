230a6f7e761035e3bde960a0e22ef6b271c972cb
Julian Cataldo <contact@juliancataldo.com>
2022-08-12 07:58:59 +0200

    feat: client helper rehaul

--- src/generate-client-helper.ts
+++ src/generate-client-helper.ts
@@ -1,28 +1,36 @@
+import chalk from 'chalk';
 import * as fs from 'node:fs/promises';
 import mkdirp from 'mkdirp';
 import _ from 'lodash-es';
 import path from 'node:path';
-import changeCase from 'change-case';
-import conf from '../config';
-
+import { pascalCase } from 'change-case';
+/* ·········································································· */
+import { conf } from './config';
 import state from './state';
+/* —————————————————————————————————————————————————————————————————————————— */
 
 export default async function generateHelper() {
-  let content = `/* AUTO-GENERATED — Do not edit! */
-import getContent from '@julian_cataldo/content-components/src/client/fetch-content';
+  const url = `http://${conf.server.host}:${conf.server.port}/content`;
 
+  let content = `/* AUTO-GENERATED — Do not edit! */
+import { timestamp } from '../.timestamp.json';
+import fetchApi from '@julian_cataldo/content-components-client/fetch-api';\n
 `;
+
   const contentSchemas = Object.entries(state.schemas.content);
 
   contentSchemas.forEach(([, schema]) => {
+    if (schema.properties === undefined) return false;
     const name = schema.title;
     const imports = `import type { ${name} } from '../types/${name}';\n`;
     content += imports;
+    return true;
   });
 
-  content += '\n';
+  content += `\nconsole.log(timestamp && '[CE] Trigger');\n\n`;
 
   contentSchemas.forEach(([key, schema]) => {
+    if (schema.properties === undefined) return false;
     const name = schema.title;
 
     const entries: string[] = [];
@@ -37,40 +45,71 @@ import getContent from '@julian_cataldo/content-components/src/client/fetch-cont
 
     if (entries.length) {
       const itemsJoined = entries.join("' | '");
-      const entriesName = `type ${name}EntryNames = '${itemsJoined}';\n`;
+      const entriesName = `export type ${name}EntryNames = '${itemsJoined}';\n`;
 
       content += entriesName;
-      content += `type ${_.startCase(_.camelCase(key))} = {
+      content += `export type ${_.startCase(_.camelCase(key))} = {
   [key in ${name}EntryNames]: ${name};
 };
 `;
+      // IDEA: re-export?
+      // content += `export type ${name};`;
     }
+    return true;
   });
 
+  const exports: string[] = [];
+
   contentSchemas.forEach(([key]) => {
+    const fnName = `get${pascalCase(key)}`;
+    const fnArrayName = `${fnName}Array`;
+    exports.push(fnName, fnArrayName);
+
     content += `
-export async function get${changeCase.pascalCase(key)}() {
-  const ${key} = await getContent('${key}');
+async function ${fnName}(): Promise<${pascalCase(key)} | false> {
+  const ${key} = await fetchApi('${url}', '${key}');
+  if (${key} === false) {
+    return false;
+  }
   if (Object.keys(${key}).length) {
-    return ${key} as unknown as ${changeCase.pascalCase(key)};
+    return ${key} as unknown as ${pascalCase(key)};
   }
   return false;
 }
-export async function get${changeCase.pascalCase(key)}Array() {
-  const ${key} = await getContent('${key}');
+async function ${fnArrayName}(): Promise<${
+      state.schemas.content[key].title
+    }[] | false>  {
+  const ${key} = await fetchApi('${url}', '${key}');
+  if (${key} === false) {
+    return false;
+  }
   if (Object.keys(${key}).length) {
     const entries = Object.entries(${key}).map(
       ([key, val]: [key: string, val: unknown]) => {
-        return { _key: key, ...(val as ${changeCase.pascalCase(key)}) };
+        return { _key: key, ...(val as ${pascalCase(key)}) };
       },
     );
-    return entries as ${changeCase.pascalCase(key)}[];
+    return entries as unknown as ${state.schemas.content[key].title}[];
   }
   return false;
 }
 `;
   });
 
+  content += `
+export default {
+  ${exports.join(',\n  ')}
+}
+`;
+
   await mkdirp(conf.helpers.dest);
-  await fs.writeFile(path.join(conf.helpers.dest, 'get-content.ts'), content);
+  const dest = path.join(conf.helpers.dest, 'get.ts');
+  await fs.writeFile(dest, content);
+
+  // eslint-disable-next-line no-console
+  console.log(
+    `${chalk.black.bgBlueBright('Helper')}(${chalk.green(
+      'generate',
+    )}): ${chalk.cyan(dest)}`,
+  );
 }
