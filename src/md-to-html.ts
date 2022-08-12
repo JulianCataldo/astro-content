6d5cd3c1b929bfc39fe07d0fb2f2f1bc62c9ea4b
Julian Cataldo <contact@juliancataldo.com>
2022-08-12 08:38:51 +0200

    feat: rehaul md to markup

--- src/md-to-html.ts
+++ src/md-to-html.ts
@@ -1,23 +1,85 @@
 import * as fs from 'node:fs/promises';
+import chalk from 'chalk';
+import yaml from 'yaml';
+/* ·········································································· */
 import { unified } from 'unified';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import rehypeFormat from 'rehype-format';
+import remarkGfm from 'remark-gfm';
 import rehypeStringify from 'rehype-stringify';
-import remarkRewrite from 'rehype-rewrite';
+import remarkFrontmatter from 'remark-frontmatter';
+import rlFmSchema from '@julian_cataldo/remark-lint-frontmatter-schema';
+import remarkDirective from 'remark-directive';
+/* ·········································································· */
+import { reporter } from 'vfile-reporter';
+import { h } from 'hastscript';
+import type { Plugin } from 'unified';
+import type { Node } from 'unist';
+import { visit } from 'unist-util-visit';
+import type { Root } from 'mdast';
+/* ·········································································· */
+import { $log } from './utils';
+// import state from './state.js';
+/* —————————————————————————————————————————————————————————————————————————— */
 
-import config from '../config';
+const directiveToNonStandardComponent: Plugin<[], Root> = () => (tree) =>
+  visit(tree, (node: Node) => {
+    if (
+      node.type === 'textDirective' ||
+      node.type === 'leafDirective' ||
+      node.type === 'containerDirective'
+    ) {
+      if (
+        node.type === 'textDirective' ||
+        node.type === 'leafDirective' ||
+        node.type === 'containerDirective'
+      ) {
+        const data = node.data || (node.data = {});
+        const hast = h(node.name, node.attributes);
 
-export default async function mdToHtml(path: string) {
+        data.hName = hast.tagName;
+        data.hProperties = hast.properties;
+      }
+    }
+  });
+
+// FIXME: this function is called too many times
+export default async function mdToHtml(path: string, schema) {
   const content = await fs.readFile(path, 'utf-8');
+
+  let frontmatter;
 
   const output = await unified()
     .use(remarkParse)
-    .use(remarkRehype)
-    .use(remarkRewrite, config.markdown.rewriteOptions)
+    .use(remarkDirective)
+    .use(directiveToNonStandardComponent)
+    .use(remarkGfm)
+    .use(remarkFrontmatter)
+    .use(rlFmSchema, {
+      embed: schema,
+    })
+    .use(() => (tree) => {
+      if (tree.children[0].type === 'yaml') {
+        frontmatter = yaml.parse(tree.children[0].value);
+      }
+    })
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeStringify, { allowDangerousHtml: true })
     .use(rehypeFormat)
-    .use(rehypeStringify)
+
     .process(content);
 
-  return String(output);
+  // eslint-disable-next-line no-console
+  console.log(
+    `${chalk.bgCyanBright('Markdown')}(${chalk.green(
+      'transform',
+    )}): ${chalk.yellow(path)}`,
+  );
+
+  output.cwd = path;
+  output.path = path;
+  $log(`\n${reporter([output])}\n`);
+
+  return { frontmatter, body: String(output) };
 }
