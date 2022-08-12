1f7dce6f7f4240b91b0823880d9cfda43c47bea4
Julian Cataldo <contact@juliancataldo.com>
2022-08-13 00:27:47 +0200

    chore: add cli stub with shebang

---
+++ cli.js
@@ -0,0 +1,8 @@
+#! /usr/bin/env node
+
+/* ——————————————————————————————————————————————————————————————————————————— *
+ *              © Julian Cataldo — https://www.juliancataldo.com.              *
+ *                      See LICENSE in the project root.                       *
+/* —————————————————————————————————————————————————————————————————————————— */
+
+import './dist/index.js';
