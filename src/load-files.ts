65be6a5131478b10a976954c9993ea71aab49a60
Julian Cataldo <contact@juliancataldo.com>
2022-08-12 08:18:27 +0200

    feat: load files rehaul

--- src/load-files.ts
+++ src/load-files.ts
@@ -1,71 +1,120 @@
 import chokidar from 'chokidar';
 import * as fs from 'node:fs/promises';
-import _ from 'lodash-es';
+import { existsSync } from 'node:fs';
+import _, { kebabCase } from 'lodash-es';
 import path from 'node:path';
 import yaml from 'js-yaml';
+import chalk from 'chalk';
+import mkdirp from 'mkdirp';
 
-import conf from '../config';
+/* ·········································································· */
+import { conf } from './config';
 import mdToHtml from './md-to-html';
 import state from './state';
+import validateData from './validate-data';
+/* —————————————————————————————————————————————————————————————————————————— */
+
+// Write dummy file for front-end framework hot-reload triggering in dev mode
+function triggerDevChange() {
+  // TODO: dev/prod mode detection
+  const data = `{ "timestamp": "${Date.now().toString()}" }\n`;
+  fs.writeFile(path.join(conf.dev.triggerFile), data);
+}
+
+async function saveBuild(type, entry, prop, data) {
+  const base = path.join(kebabCase(type), kebabCase(entry));
+  const dir = path.join(conf.components.dest, base);
+  await mkdirp(dir);
+  const filePath = path.join(dir, `${kebabCase(prop)}.json`);
+  fs.writeFile(filePath, JSON.stringify(data, null, 2));
+}
+
+function loadFile(eventName: string, schemaPath: string) {
+  const parts = path.relative(conf.components.src, schemaPath) || schemaPath;
+
+  const objectPath = path.dirname(parts);
+  const c = state.content[objectPath];
+  const isSingleton = c?.type === 'singleton';
+
+  const typePath = isSingleton
+    ? path.dirname(parts)
+    : path.dirname(path.dirname(parts));
+
+  const contentState = _.get(state.content, typePath.replaceAll('/', '.'));
+
+  const schema = state.schemas.content[typePath];
+
+  const base = path.join(conf.components.src, path.dirname(parts));
+
+  if (schema?.properties === undefined) return false;
+
+  Object.entries(schema.properties)?.forEach(async ([key, val]) => {
+    const collection = _.camelCase(path.basename(objectPath));
+
+    const targetProp = _.kebabCase(key);
+    const currentProp = path.basename(parts, path.extname(parts));
+    if (targetProp !== currentProp) return false;
+
+    let data;
+
+    const pathNoExt = path.join(base, `${_.kebabCase(key)}`);
+
+    const mdPath = `${pathNoExt}.md`;
+    const mdExists = existsSync(mdPath);
+    if (mdExists) {
+      const md = await mdToHtml(mdPath, val.properties.frontmatter);
+
+      data = {
+        headings: 'yolo',
+        excerpt: 'yo',
+        frontmatter: md.frontmatter,
+        body: md.body,
+      };
+    } else if (val.type === 'object') {
+      const file = await fs
+        .readFile(`${pathNoExt}.yaml`, 'utf-8')
+        .catch(() => null);
+
+      if (file) {
+        data = yaml.load(file);
+      }
+    }
+
+    if (data) {
+      validateData(typePath, collection, key, data);
+
+      if (isSingleton) {
+        contentState.data[key] = data;
+        saveBuild(typePath, null, key, data);
+      } else {
+        contentState.items[collection] = {
+          ...contentState.items[collection],
+          [key]: data,
+        };
+        saveBuild(typePath, collection, key, data);
+      }
+
+      triggerDevChange();
+      // eslint-disable-next-line no-console
+      console.log(
+        `Content(${chalk.green(eventName)}): ${chalk.yellow(schemaPath)}`,
+      );
+    }
+    return true;
+  });
+
+  return true;
+}
 
 export default async function loadFiles() {
   chokidar
-    .watch(`${conf.components.src}/**/*.{md,yaml}`)
+    .watch(path.join(conf.components.src, `**/*.{md,yaml}`), {
+      ignored: ['node_modules', '**/*.schema.yaml', '**/*.log.yaml'],
+    })
     .on('all', (eventName, schemaPath) => {
       if (['add', 'change'].includes(eventName)) {
-        const parts = path.relative(conf.components.src, schemaPath);
-
-        const typePath = path.dirname(path.dirname(parts));
-        const objectPath = path.dirname(parts);
-
-        const currentType = _.get(state.content, typePath.replaceAll('/', '.'));
-
-        if (currentType?.type === 'collection') {
-          const schema = state.schemas.content[typePath];
-
-          Object.entries(schema.properties)?.forEach(async ([key, val]) => {
-            const base = path.join(conf.components.src, path.dirname(parts));
-
-            const collection = _.camelCase(path.basename(objectPath));
-
-            if (val['$ref']?.includes('markdown.schema')) {
-              const mdPath = path.join(base, `${_.kebabCase(key)}.md`);
-
-              const file = await fs.readFile(mdPath, 'utf-8').catch(() => null);
-
-              schema.properties[key] = state.schemas.internals.MarkdownFile;
-
-              if (file) {
-                const result = await mdToHtml(mdPath);
-
-                const data = {
-                  headings: 'yolo',
-                  excerpt: 'yo',
-                  body: result,
-                };
-                currentType.items[collection] = {
-                  ...currentType.items[collection],
-                  [key]: data,
-                };
-              }
-            } else if (val.type === 'object') {
-              const yamlPath = path.join(base, `${_.kebabCase(key)}.yaml`);
-
-              const file = await fs
-                .readFile(yamlPath, 'utf-8')
-                .catch(() => null);
-
-              if (file) {
-                const data = yaml.load(file);
-
-                currentType.items[collection] = {
-                  ...currentType.items[collection],
-                  [key]: data,
-                };
-              }
-            }
-          });
-        }
+        loadFile(eventName, schemaPath);
       }
+      return true;
     });
 }
