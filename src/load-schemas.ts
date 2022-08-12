92a0ee3ec8aa6df7303695ed7b377aed71767ada
Julian Cataldo <contact@juliancataldo.com>
2022-08-12 08:06:04 +0200

    feat: schema loader rehaul

--- src/load-schemas.ts
+++ src/load-schemas.ts
@@ -4,13 +4,16 @@ import path from 'node:path';
 import chokidar from 'chokidar';
 import yaml from 'js-yaml';
 import _ from 'lodash-es';
-
+import chalk from 'chalk';
+import { pascalCase } from 'change-case';
 import { compile, JSONSchema } from 'json-schema-to-typescript';
-
+/* ·········································································· */
 import state from './state';
-
-import conf from '../config';
+import { conf } from './config';
 import generateHelper from './generate-client-helper';
+import updateVsCode from './update-vscode';
+import { $log } from './utils';
+/* —————————————————————————————————————————————————————————————————————————— */
 
 async function updateSchema({ schemaPath }: { schemaPath: string }) {
   const parts = path.relative(conf.components.src, schemaPath);
@@ -25,61 +28,98 @@ async function updateSchema({ schemaPath }: { schemaPath: string }) {
   }
 
   const parent = path.dirname(parts);
+
   const pathParsed = parent.replaceAll('/', '.');
 
   const contentIsUnset = _.get(state.content, pathParsed) === undefined;
   if (contentIsUnset) {
-    let data = {};
-    if (isSingleton) {
-      data = {
-        type: 'singleton',
-        data: {},
-      };
-    } else {
-      data = {
-        type: 'collection',
-        items: {},
-      };
-    }
+    const data = isSingleton
+      ? { type: 'singleton', data: {} }
+      : { type: 'collection', items: {} };
+
     _.set(state.content, pathParsed, data);
+
+    $log(
+      `${chalk.bgMagenta('Schemas')}(${pathParsed}): ${chalk.yellow(
+        'Content structure updated',
+      )}`,
+    );
   }
 
-  const directory = conf.types.dest;
-  await mkdirp(directory);
-  state.schemas.content[parent] = schema;
+  // FIXME: avoid setTimeout
+  setTimeout(() => updateVsCode(), 50);
 
+  const directory = conf.types.dest;
+
+  await mkdirp(directory);
+  state.schemas.content[parent] = { title: pascalCase(schemaName), ...schema };
+
+  // FIXME: avoid setTimeout
   setTimeout(() => {
     const destination = path.join(directory, `${schemaName}.ts`);
 
-    compile(state.schemas.content[parent], 'untitled')
-      .then((ts) => fs.writeFile(destination, ts))
-      .catch(() => null);
+    const schemaForTs = {
+      definitions: { ...state.schemas.internals },
+      ...state.schemas.content[parent],
+    };
+
+    compile(schemaForTs, 'untitled')
+      .then((ts) => {
+        fs.writeFile(destination, ts);
+      })
+      .catch((e) => console.error(e));
   }, 2000);
 
   generateHelper();
 }
 
 async function watchFilesSchemas() {
+  // IDEA: throttle all files writes, using last detected change?
+
+  const schemasGlob = path.join(
+    process.cwd(),
+    conf.components.src,
+    `**/*.schema.yaml`,
+  );
+
   chokidar
-    .watch(`${conf.components.src}/**/*.schema.yaml`)
+    .watch(schemasGlob, {
+      ignored: ['**/node_modules'],
+    })
     .on('all', async (eventName, schemaPath) => {
       if (['add', 'change'].includes(eventName)) {
-        updateSchema({ schemaPath });
+        // eslint-disable-next-line no-console
+        $log(
+          `${chalk.black.bgMagenta('Schemas')}(${chalk.green(
+            eventName,
+          )}): ${chalk.yellow(schemaPath)}`,
+        );
+        await updateSchema({ schemaPath });
       }
     });
 }
 
 export default async function loadSchemas() {
-  const internalSchemas = {
-    MarkdownFile: yaml.load(
-      await fs.readFile('./schemas/markdown.schema.yaml', 'utf-8'),
-    ),
-  };
-
   // REFACTOR: Move watchers in a central place?
   watchFilesSchemas();
 
   state.schemas.internals = {
-    ...internalSchemas,
+    MarkdownFile: {
+      title: 'Markdown file',
+      type: 'object',
+      properties: {
+        headings: {
+          type: 'string',
+        },
+        body: {
+          type: 'string',
+        },
+        excerpt: {
+          type: 'string',
+        },
+      },
+      required: ['body'],
+      additionalProperties: false,
+    },
   };
 }
