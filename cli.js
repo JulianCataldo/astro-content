c01c5f61316c1b10a670ca089c23ddc2683b94b0
Julian Cataldo <contact@juliancataldo.com>
2022-08-13 08:20:17 +0200

    feat: add cli version greeting

--- cli.js
+++ cli.js
@@ -1,8 +1,13 @@
-#! /usr/bin/env node
+#! /usr/bin/env node --no-warnings
 
 /* ——————————————————————————————————————————————————————————————————————————— *
  *              © Julian Cataldo — https://www.juliancataldo.com.              *
  *                      See LICENSE in the project root.                       *
+/* —————————————————————————————————————————————————————————————————————————— */
+
+import pkg from './package.json' assert { type: 'json' };
+console.log(`Maestro — CLI — ${pkg.version}`);
+
 /* —————————————————————————————————————————————————————————————————————————— */
 
 import './dist/index.js';
