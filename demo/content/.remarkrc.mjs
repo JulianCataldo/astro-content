2972dd9751149501983fc9e6814bd8f233724934
Julian Cataldo <contact@juliancataldo.com>
2022-08-13 08:27:16 +0200

    feat: reflect auto-schema loading

--- demo/content/.remarkrc.mjs
+++ demo/content/.remarkrc.mjs
@@ -1,9 +1,11 @@
+/* —————————————————————————————————————————————————————————————————————————— */
 import remarkFrontmatter from 'remark-frontmatter';
 import remarkLintFrontmatterSchema from '@julian_cataldo/remark-lint-frontmatter-schema';
-
 import remarkPresetLintRecommended from 'remark-preset-lint-recommended';
 import remarkPresetLintMarkdownStyleGuide from 'remark-preset-lint-markdown-style-guide';
 import remarkPresetLintConsistent from 'remark-preset-lint-consistent';
+/* ·········································································· */
+import schemas from './.ccomp/schemas/vscode/schemas.mjs';
 /* —————————————————————————————————————————————————————————————————————————— */
 
 const remarkConfig = {
@@ -12,16 +14,7 @@ const remarkConfig = {
     remarkPresetLintMarkdownStyleGuide,
     remarkPresetLintConsistent,
     remarkFrontmatter,
-    [
-      remarkLintFrontmatterSchema,
-      {
-        schemas: {
-          './.ccomp/schemas/vscode/articles-main.schema.yaml': [
-            'content/articles/*/main.md',
-          ],
-        },
-      },
-    ],
+    [remarkLintFrontmatterSchema, { schemas }],
   ],
 };
 
