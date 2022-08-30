59f5ff224e323bea066eaf673a1e78ca2bb64127
Julian Cataldo <contact@juliancataldo.com>
2022-08-30 10:36:32 +0200

    feat: init docs project

---
+++ docs/astro.config.ts
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'astro/config';
+/* ·········································································· */
+import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
+import content from 'astro-content';
+
+/* —————————————————————————————————————————————————————————————————————————— */
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://example.com',
+  server: {
+    port: 9054,
+    host: true,
+  },
+  integrations: [
+    // v————— Base: CLI + Server
+    content(),
+    // v————— (Optional) For GUI
+    react(),
+    mdx(),
+  ],
+});
