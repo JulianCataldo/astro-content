33f0f4919234c388de2a2b410104ae707f8717f5
Julian Cataldo <contact@juliancataldo.com>
2022-09-01 03:07:57 +0200

    feat: add cli shortcuts to "create"

--- packages/gui/components/Toolbar.tsx
+++ packages/gui/components/Toolbar.tsx
@@ -11,10 +11,25 @@ import { useAppStore } from '../store';
 
 export default function Entity() {
   const { content, schemas, config } = useAppStore((state) => state.data);
-  const { entity, entry, property } = useAppStore((state) => state.route);
+  const { entity, entry, property } = useAppStore(
+    (state) => state.uiState.route,
+  );
 
   const setRoute = useAppStore((state) => state.setRoute);
   const save = useAppStore((state) => state.save);
+
+  const value = content?.[entity]?.[entry]?.[property];
+  const isMd = value?.headings && value?.rawMd;
+
+  const copy = async (e) => {
+    await navigator.clipboard.writeText(e.target.innerText);
+    const prev = e.target.innerText;
+    e.target.innerText = 'Copied!';
+    setTimeout(() => {
+      e.target.innerText = prev;
+    }, 1500);
+    console.log();
+  };
 
   return (
     <div className="component-toolbar">
@@ -22,13 +37,37 @@ export default function Entity() {
         <ModalPopover
           render={({ close, labelId, descriptionId }) => (
             <>
-              <div>
-                <h3 id={labelId}>Create new app</h3>
+              {/* <div>
                 <p id={descriptionId}>Keep the name short!</p>
                 <input placeholder="Name" autoFocus />
                 <button onClick={close}>Create</button>
-              </div>
-              <code>&gt; $ maestro create entity entry</code>
+              </div> */}
+              <h3 id={labelId}>From command line</h3>
+
+              <h4>Define a new collection</h4>
+              <pre>
+                <span className="prefix">&gt; </span>
+                <code onClick={copy}>pnpm content add people person</code>
+              </pre>
+              {/*
+              <h4>New singleton</h4>
+              <pre>
+                <code>&gt; pnpm content add contact-page</code>
+              </pre>
+              */}
+
+              <h4>-or- for an existing collection</h4>
+              <h5>Add an entry</h5>
+              <pre>
+                <span className="prefix">&gt; </span>
+                <code onClick={copy}>pnpm content add people jane</code>
+              </pre>
+
+              <h5>Add an entry with a random name</h5>
+              <pre>
+                <span className="prefix">&gt; </span>
+                <code onClick={copy}>pnpm content add people</code>
+              </pre>
             </>
           )}
         >
@@ -40,9 +79,12 @@ export default function Entity() {
       </div>
 
       <div className="actions file">
-        <div className="action" onClick={save}>
-          <Icon icon="system-uicons:floppy" width="2em" /> Save
-        </div>
+        {entity && (
+          <div className="action" onClick={save}>
+            <Icon icon="system-uicons:floppy" width="2em" /> Save
+          </div>
+        )}
+
         <div className="breadcrumb">
           {entity && schemas && schemas.content && schemas.content[entity] && (
             <div
@@ -66,9 +108,9 @@ export default function Entity() {
             </div>
           )}
           {property && (
-            <div>
+            <div className={cx('chevron-property', isMd ? 'md' : 'yaml')}>
               <Icon icon="system-uicons:chevron-right" width="2em" />
-              {sentenceCase(property)}
+              <span>{sentenceCase(property)}</span>
             </div>
           )}
         </div>
@@ -76,6 +118,30 @@ export default function Entity() {
 
       {content?.[entity]?.[entry]?.[property] && (
         <div className="actions">
+          {content[entity][entry][property].headings &&
+            content[entity][entry][property].rawMd && (
+              <div className="headings">
+                <ModalPopover
+                  render={({ close, labelId, descriptionId }) => (
+                    <div className="headings-details">
+                      <Headings
+                        items={content[entity][entry][property]?.headings}
+                      />
+                    </div>
+                  )}
+                >
+                  <div className="action">
+                    <Icon
+                      icon="system-uicons:document-list"
+                      width="2em"
+                      height="2.5em"
+                    />
+                    Headings
+                  </div>
+                </ModalPopover>
+              </div>
+            )}
+
           <a
             target="content-maestro-preview"
             href={config?.previewUrl}
@@ -88,27 +154,6 @@ export default function Entity() {
           <div className="action">
             <Icon icon="system-uicons:info-circle" width="2em" height="1.5em" />
             Infos
-          </div>
-
-          <div className="headings">
-            <ModalPopover
-              render={({ close, labelId, descriptionId }) => (
-                <div className="headings-details">
-                  <Headings
-                    items={content[entity][entry][property]?.headings}
-                  />
-                </div>
-              )}
-            >
-              <div className="action">
-                <Icon
-                  icon="system-uicons:document-list"
-                  width="2em"
-                  height="2.5em"
-                />
-                Headings
-              </div>
-            </ModalPopover>
           </div>
         </div>
       )}
