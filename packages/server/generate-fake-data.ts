75fd0b7aa7b967db4b392d11837265f2b5fb6fe0
Julian Cataldo <contact@juliancataldo.com>
2022-09-08 12:34:49 +0200

    feat: schema generated fake data live preview

---
+++ packages/server/generate-fake-data.ts
@@ -0,0 +1,16 @@
+import jsf from 'json-schema-faker';
+import type { JSONSchema7 } from 'json-schema';
+/* ·········································································· */
+import { log } from './logger';
+/* —————————————————————————————————————————————————————————————————————————— */
+
+export async function generateFakeEntries(schema: JSONSchema7) {
+  const fakeEntries = await jsf.resolve(schema).catch((e) => {
+    log(e, 'absurd');
+    return {};
+  });
+
+  log({ schema, fakeEntries }, 'absurd');
+
+  return fakeEntries;
+}
