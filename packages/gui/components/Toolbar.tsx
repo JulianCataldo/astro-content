db8c046f49c11c3eb08d2d57608fe0ff44d59ccd
Julian Cataldo <contact@juliancataldo.com>
2022-09-08 13:23:27 +0200

    feat: copy inline code, more infos, open in ide

--- packages/gui/components/Toolbar.tsx
+++ packages/gui/components/Toolbar.tsx
@@ -1,37 +1,33 @@
+// REFACTOR: Split in sub-components
+/* eslint-disable max-lines */
+
 import { Icon } from '@iconify/react';
 import { sentenceCase } from 'change-case';
 import cx from 'classnames';
 /* ·········································································· */
 import Headings from './Headings';
 import ModalPopover from './ModalPopover';
-/* ·········································································· */
 import './Toolbar.scss';
-import { useAppStore } from '../store';
 import Tooltip from './Tooltip';
+/* ·········································································· */
+import useAppStore from '../store';
+import CopyInlineCode from './CopyInlineCode';
+
 /* —————————————————————————————————————————————————————————————————————————— */
 
-export default function Entity() {
+export default function Toolbar() {
   const { content, schemas, config } = useAppStore((state) => state.data);
-  const { entity, entry, property } = useAppStore(
-    (state) => state.uiState.route,
-  );
+  const {
+    route: { entity, entry, property },
+    language,
+  } = useAppStore((state) => state.uiState);
 
   const setRoute = useAppStore((state) => state.setRoute);
   const save = useAppStore((state) => state.save);
 
-  const value =
-    entity && entry && property && content?.[entity]?.[entry]?.[property];
-  const isMd = value?.headings && value?.rawMd;
-
-  const copy = async (e) => {
-    await navigator.clipboard.writeText(e.target.innerText);
-    const prev = e.target.innerText;
-    e.target.innerText = 'Copied!';
-    setTimeout(() => {
-      e.target.innerText = prev;
-    }, 1500);
-    console.log();
-  };
+  const isMd = language === 'markdown';
+  const fullPath =
+    entity && entry && property && content[entity]?.[entry]?.[property]?.file;
 
   return (
     <div className="component-toolbar">
@@ -39,37 +35,41 @@ export default function Entity() {
         <ModalPopover
           render={({ close, labelId, descriptionId }) => (
             <>
-              {/* <div>
+              {/* 
+              <div>
                 <p id={descriptionId}>Keep the name short!</p>
                 <input placeholder="Name" autoFocus />
                 <button onClick={close}>Create</button>
-              </div> */}
+              </div>
+              */}
               <h3 id={labelId}>From command line</h3>
 
               <h4>Define a new collection</h4>
-              <pre>
-                <span className="prefix">&gt; </span>
-                <code onClick={copy}>pnpm content add people person</code>
-              </pre>
+
+              <CopyInlineCode
+                text="pnpm content add people person"
+                placement="right"
+              />
+
               {/*
-              <h4>New singleton</h4>
-              <pre>
+                <h4>New singleton</h4>
                 <code>&gt; pnpm content add contact-page</code>
-              </pre>
               */}
 
               <h4>-or- for an existing collection</h4>
               <h5>Add an entry</h5>
-              <pre>
-                <span className="prefix">&gt; </span>
-                <code onClick={copy}>pnpm content add people jane</code>
-              </pre>
+
+              <CopyInlineCode
+                text="pnpm content add people jane"
+                placement="right"
+              />
 
               <h5>Add an entry with a random name</h5>
-              <pre>
-                <span className="prefix">&gt; </span>
-                <code onClick={copy}>pnpm content add people</code>
-              </pre>
+
+              <CopyInlineCode
+                text="pnpm content add people"
+                placement="right"
+              />
             </>
           )}
         >
@@ -82,19 +82,24 @@ export default function Entity() {
 
       <div className="actions file">
         {entity && (
+          // FIXME: JSX A11y
+          // eslint-disable-next-line jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events
           <div className="action" onClick={save}>
             <Icon icon="system-uicons:floppy" width="2em" /> Save
           </div>
         )}
 
         <div className="breadcrumb">
-          {entity && schemas && schemas.content && schemas.content[entity] && (
+          {entity && (
+            // FIXME: JSX A11y
+            // eslint-disable-next-line jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events
             <div
-              onClick={(e) => setRoute(entity, false, false)}
+              onClick={() => setRoute(entity, false, false)}
               className={cx('entity-link', entry && 'action')}
             >
               <Icon icon="system-uicons:location" width="2em" />
-              {sentenceCase(schemas.content[entity].title)}
+              {typeof schemas.content[entity] === 'object' &&
+                sentenceCase(schemas.content[entity].title ?? '')}
             </div>
           )}
           {entity && !entry && (
@@ -115,19 +120,25 @@ export default function Entity() {
               <span>{sentenceCase(property)}</span>
             </div>
           )}
+          {/* ({language}) */}
         </div>
       </div>
 
-      {content?.[entity]?.[entry]?.[property] && (
-        <div className="actions">
-          {content[entity][entry][property].headings &&
-            content[entity][entry][property].rawMd && (
+      {entity &&
+        entry &&
+        property &&
+        typeof content[entity]?.[entry]?.[property] === 'object' && (
+          <div className="actions">
+            {isMd && (
               <div className="headings">
                 <ModalPopover
                   render={({ close, labelId, descriptionId }) => (
                     <div className="headings-details">
+                      {/* FIXME: */}
                       <Headings
-                        items={content[entity][entry][property]?.headings}
+                        items={
+                          content[entity]?.[entry]?.[property]?.headingsCompiled
+                        }
                       />
                     </div>
                   )}
@@ -144,36 +155,70 @@ export default function Entity() {
               </div>
             )}
 
-          <a
-            target="content-maestro-preview"
-            href={config?.previewUrl}
-            className="action"
-          >
-            <Icon icon="simple-icons:astro" width="2em" height="1.5em" />
-            Preview
-          </a>
+            <a
+              target="content-maestro-preview"
+              href={config.previewUrl}
+              className="action"
+            >
+              <Icon icon="simple-icons:astro" width="2em" height="1.5em" />
+              Preview
+            </a>
 
-          <Tooltip
-            label={
-              <>
-                {/* {JSON.stringify(content[entity][entry][property])} */}
-                {content[entity][entry][property].file}
-              </>
-            }
-            jsx
-            placement="top"
-          >
-            <div className="action">
-              <Icon
-                icon="system-uicons:info-circle"
-                width="2em"
-                height="1.5em"
-              />
-              <span>Infos</span>
-            </div>
-          </Tooltip>
-        </div>
-      )}
+            <Tooltip
+              label={
+                <div className="infos-tooltip">
+                  {/* {JSON.stringify(content[entity][entry][property])} */}
+                  {fullPath && (
+                    <div>
+                      <strong>Current file</strong>:{fullPath}
+                      <hr />
+                    </div>
+                  )}
+                  <div>
+                    {Object.entries(import.meta.env).map(([key, obj]) =>
+                      obj && !['DEV', 'PROD'].includes(key) ? (
+                        <div className="table" key={key}>
+                          <span>
+                            <strong>{key}</strong>:
+                          </span>
+                          <span> {JSON.stringify(obj)}</span>
+                        </div>
+                      ) : null,
+                    )}
+                  </div>
+                </div>
+              }
+              jsx
+              placement="top"
+            >
+              <div className="action">
+                <Icon
+                  icon="system-uicons:info-circle"
+                  width="2em"
+                  height="1.5em"
+                />
+                <span>Infos</span>
+              </div>
+            </Tooltip>
+
+            {fullPath && (
+              <Tooltip
+                label={<>Open in VS Code: {fullPath}</>}
+                jsx
+                placement="top"
+              >
+                <a className="action" href={`vscode://${fullPath}`}>
+                  <Icon
+                    icon="simple-icons:visualstudiocode"
+                    width="2em"
+                    height="1.5em"
+                  />
+                  <span>IDE</span>
+                </a>
+              </Tooltip>
+            )}
+          </div>
+        )}
     </div>
   );
 }
