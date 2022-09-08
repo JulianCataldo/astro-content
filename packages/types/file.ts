aefb43c4c208062619234fe995a87c4a7918f9dc
Julian Cataldo <contact@juliancataldo.com>
2022-09-08 12:19:30 +0200

    feat: extend file instance types

--- packages/types/file.ts
+++ packages/types/file.ts
@@ -1,3 +1,23 @@
+import type { MarkdownHeading, MarkdownInstance } from 'astro';
+
+export type FileInstance =
+  | (MarkdownInstance<Record<string, unknown>> & ExtraMd)
+  | (YamlInstance<unknown> & ExtraYaml);
+
+export interface ExtraMd {
+  rawMd: string;
+  bodyCompiled?: string;
+  headingsCompiled?: MarkdownHeading[];
+  excerpt?: { html: string };
+  //   footnotes?: Footnotes;
+  //   rawYaml?: string;
+  //   data: unknown;
+  //   file: unknown
+}
+export interface ExtraYaml {
+  // rawYaml: string;
+}
+
 export interface YamlInstance<T> {
   data: T;
   /** Absolute file path (e.g. `/home/user/projects/.../file.yaml`) */
