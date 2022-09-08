66c15c1ae43917b602e8b2434534a160eff9eba5
Julian Cataldo <contact@juliancataldo.com>
2022-09-08 12:18:47 +0200

    feat: add validate and fake dto

--- packages/types/dto.ts
+++ packages/types/dto.ts
@@ -1,7 +1,22 @@
-export interface SaveDTO {
-  entity: string | null;
-  entry: string | null;
-  property: string | null;
+import type { JSONSchema7 } from 'json-schema';
+import type { Part } from './gui-state';
+
+export interface Save {
+  file: string;
+  singular: string;
   value: string;
-  language: string;
+  language: string | null;
+}
+
+export interface Validate {
+  entity: Part;
+  entry: Part;
+  property: Part;
+  schema: JSONSchema7;
+  value: string;
+  language: string | null;
+}
+
+export interface Fake {
+  schema: JSONSchema7;
 }
