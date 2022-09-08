bebe1200fc68eeb029721ccd206ca0b114945d83
Julian Cataldo <contact@juliancataldo.com>
2022-09-08 12:32:07 +0200

    refactor: simplify save, using original file path

--- packages/server/save.ts
+++ packages/server/save.ts
@@ -1,25 +1,10 @@
-import { kebabCase } from 'lodash-es';
-import * as path from 'node:path';
 import fs from 'node:fs/promises';
-import type { SaveDTO } from '@astro-content/types/dto';
 /* ·········································································· */
+import type { Save } from '@astro-content/types/dto';
+import { log } from './logger';
 /* —————————————————————————————————————————————————————————————————————————— */
 
-const extensions: Record<string, string> = {
-  markdown: '.md',
-  yaml: '.yaml',
-  // 'mdx': '.mdx',
-};
-
-export default async function saveDTO(object: SaveDTO) {
-  if (object.entity && object.entry && object.property) {
-    const dest = path.join(
-      process.cwd(),
-      'content',
-      kebabCase(object.entity),
-      kebabCase(object.entry),
-      `${kebabCase(object.property)}${extensions[object.language]}`,
-    );
-    await fs.writeFile(dest, object.value);
-  }
+export async function save(object: Save) {
+  const dest = object.file;
+  await fs.writeFile(dest, object.value).catch((e) => log(e));
 }
