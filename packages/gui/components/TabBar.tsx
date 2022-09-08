448849d6ceb18e741148ded85dba5705fda8bbc3
Julian Cataldo <contact@juliancataldo.com>
2022-09-08 13:24:49 +0200

    feat: default tab

--- packages/gui/components/TabBar.tsx
+++ packages/gui/components/TabBar.tsx
@@ -4,27 +4,48 @@ import cx from 'classnames';
 import './TabBar.scss';
 /* —————————————————————————————————————————————————————————————————————————— */
 
-export type Tabs = Record<string, { button: { icon?: string; title: string } }>;
+export type Tabs = Record<string, { icon?: string; title: string }>;
 
 interface Props {
   tabs: Tabs;
   switchPane: (index: string) => void;
   currentTab: string;
+  defaultTab: string;
 }
-export default function Entity({ tabs, switchPane, currentTab }: Props) {
+export default function Entity({
+  tabs,
+  switchPane,
+  currentTab,
+  defaultTab,
+}: Props) {
+  let hasTab = false;
+  Object.entries(tabs).forEach(([index /* , tab */]) => {
+    if (currentTab === index) {
+      hasTab = true;
+    }
+  });
+
   return (
     <div className="component-tab-bar">
+      {/* <div className="scroller"> */}
       {Object.entries(tabs).map(([index, tab]) => (
+        // FIXME: JSX A11y
+        // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
         <div
           key={index}
-          className={cx(currentTab === index && 'active', 'tab')}
+          className={cx(
+            (currentTab === index || (!hasTab && index === defaultTab)) &&
+              'active',
+            'tab',
+          )}
           onClick={() => switchPane(index)}
         >
-          {tab.button.icon && <Icon icon={tab.button.icon} />}
+          {tab.icon && <Icon icon={tab.icon} height="1.3em" width="1.3em" />}
 
-          <span className="tab-label"> {tab?.button?.title}</span>
+          <span className="tab-label"> {tab.title}</span>
         </div>
       ))}
+      {/* </div> */}
     </div>
   );
 }
