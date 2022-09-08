17d7547952613b6d4ee6885ff6099b8328515af5
Julian Cataldo <contact@juliancataldo.com>
2022-09-08 12:42:11 +0200

    feat: add new report types to file handler

--- packages/server/handle-md.ts
+++ packages/server/handle-md.ts
@@ -1,91 +1,133 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
-import type { MarkdownInstance } from 'astro';
 import prettier from 'prettier';
 /* ·········································································· */
-import type { YamlInstance } from '@astro-content/types/file';
-import state from './state';
-import validateData from './validate-yaml';
-import validateMd from './validate-md';
+import type { MarkdownInstance } from 'astro';
+import type { FileInstance, YamlInstance } from '@astro-content/types/file';
+import { state } from './state';
+import { validateYaml } from './validate-yaml';
+import { validateMd } from './validate-md';
 import { getTrio } from './utils';
+import generateExcerpt from './generate-excerpt';
+import { log } from './logger';
 /* —————————————————————————————————————————————————————————————————————————— */
 
-export default async function handleMd(
+export async function handleMd(
   filePath: string,
-  file: MarkdownInstance<Record<string, unknown>>[] | YamlInstance<unknown>[],
+  file: FileInstance,
+  editMode = false,
 ) {
-  // FOR DEBUG ——v
-  // state.content = {};
-
   const { first: entity, second: entry, third: property } = getTrio(filePath);
 
-  if (
-    entity &&
-    property &&
-    state.schemas.content[entity]?.properties?.[property]
-  ) {
-    let collected;
+  if (property && state.schemas.content[entity].properties?.[property]) {
+    let collected: FileInstance | null = null;
 
     if (state.content[entity] === undefined) {
       state.content[entity] = {};
     }
-    if (state.content[entity][entry] === undefined) {
-      state.content[entity][entry] = {};
+    if (state.content[entity]?.[entry] === undefined) {
+      state.content[entity] = {
+        ...state.content[entity],
+        [entry]: {},
+      };
     }
 
     if (filePath.endsWith('yaml') || filePath.endsWith('yml')) {
-      collected = { data: file.data, rawYaml: file.rawYaml };
-
-      validateData(entity, entry, property, collected.data, collected.rawYaml);
-    }
-    if (filePath.endsWith('md')) {
-      const rawMd = await fs.readFile(filePath, 'utf8');
-      // const rawMd = 'sdfsdf';
+      const yamlFile = file as YamlInstance<unknown>;
       collected = {
-        ...file,
+        data: yamlFile.data,
+        rawYaml: yamlFile.rawYaml,
         file: path.relative(process.cwd(), filePath),
-        headings: file.getHeadings(),
-        rawMd,
-        body: prettier.format(file.compiledContent(), { parser: 'html' }),
       };
 
-      validateMd(
-        rawMd,
-        state.schemas.content[entity].properties[property].allOf[1]?.properties
-          .frontmatter || {},
-      )
-        .then((errs) => {
-          if (state.errors[entity] === undefined) {
-            state.errors[entity] = {};
-          }
-          if (state.errors[entity][entry] === undefined) {
-            state.errors[entity] = { ...state.errors[entity], [entry]: {} };
-          }
-          if (state.errors[entity][entry][property] === undefined) {
-            state.errors[entity] = {
-              ...state.errors[entity],
-              [entry]: {
-                ...state.errors[entity][entry],
-                [property]: { schema: [], lint: [], prose: [] },
-              },
-            };
-          }
-          state.errors[entity][entry][property].lint = errs.lint;
-          state.errors[entity][entry][property].schema = errs.schema;
-          state.errors[entity][entry][property].prose = errs.prose;
-        })
-        .then(() => null)
-        .catch(() => null);
+      validateYaml(entity, entry, property, collected.rawYaml);
     }
-    // if (filePath.endsWith('mdx')) {
-    //   const rawMd = await fs.readFile(filePath, 'utf8');
-    //   collected = {
-    //     ...file,
-    //     file: undefined,
-    //     headings: file.getHeadings(),
-    //     rawMd,
-    //   };
-    // }
-    state.content[entity][entry][property] = collected;
+    if (filePath.endsWith('md')) {
+      const mdFile = file as MarkdownInstance<Record<string, unknown>>;
+
+      collected = {
+        ...mdFile,
+        // For YAML, remove this
+        file: path.relative(process.cwd(), filePath),
+      };
+
+      // collected.Content = file?.Content;
+
+      if (editMode) {
+        const rawMd = await fs.readFile(filePath, 'utf8');
+        collected.headingsCompiled = mdFile.getHeadings();
+        collected.rawMd = rawMd;
+        collected.bodyCompiled = prettier.format(mdFile.compiledContent(), {
+          parser: 'html',
+        });
+        collected.excerpt = await generateExcerpt(rawMd);
+
+        log({ file, collected }, 'absurd');
+
+        // NO VALIDATION outside edit-mode for now
+        const propSchema =
+          typeof state.schemas.content[entity] === 'object' &&
+          state.schemas.content[entity].properties?.[property];
+
+        if (typeof propSchema !== 'object') {
+          return false;
+        }
+        if (Array.isArray(propSchema.allOf)) {
+          log({ property });
+          const frontmatterSchema =
+            typeof propSchema.allOf[1] === 'object' &&
+            typeof propSchema.allOf[1]?.properties?.frontmatter === 'object' &&
+            propSchema.allOf[1]?.properties?.frontmatter;
+
+          log(frontmatterSchema);
+
+          validateMd(rawMd, frontmatterSchema || {})
+            .then((report) => {
+              if (state.errors[entity] === undefined) {
+                state.errors[entity] = {};
+              }
+
+              if (state.errors[entity]?.[entry] === undefined) {
+                state.errors[entity] = { ...state.errors[entity], [entry]: {} };
+              }
+              if (state.errors[entity]?.[entry]?.[property] === undefined) {
+                state.errors[entity] = {
+                  ...state.errors[entity],
+                  [entry]: {
+                    ...state.errors[entity][entry],
+                    [property]: {
+                      schema: [],
+                      lint: [],
+                      prose: [],
+                      footnotes: [],
+                      links: [],
+                    },
+                  },
+                };
+              }
+              state.errors[entity][entry][property] = report;
+            })
+            .then(() => null)
+            .catch(() => null);
+        }
+        // if (filePath.endsWith('mdx')) {
+        //   const rawMd = await fs.readFile(filePath, 'utf8');
+        //   collected = {
+        //     ...file,
+        //     file: undefined,
+        //     headings: file.getHeadings(),
+        //     rawMd,
+        //   };
+        // }
+      }
+    }
+
+    log({ collected }, 'absurd');
+
+    // FIXME:
+    // if  (state.content[entity]?.[entry]?.[property])
+    if (collected && typeof state.content[entity]?.[entry] === 'object') {
+      state.content[entity][entry][property] = collected;
+    }
   }
 }
