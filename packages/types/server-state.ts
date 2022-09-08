a42e307b94e599f962b81473f77f30bde21d6007
Julian Cataldo <contact@juliancataldo.com>
2022-09-08 12:21:18 +0200

    feat: add serve state types

--- packages/types/server-state.ts
+++ packages/types/server-state.ts
@@ -1,17 +1,46 @@
 /* eslint-disable @typescript-eslint/consistent-indexed-object-style */
 import type { JSONSchema7 } from 'json-schema';
-import type { Position } from 'unist';
+import type { Position as UnistPosition } from 'unist';
+import type {
+  FootnoteDefinition as MdAstFootnoteDefinition,
+  FootnoteReference as MdAstFootnoteReference,
+  Link,
+} from 'mdast';
+import type { ErrorObject as AjvErrorObject } from 'ajv';
+import type { VFileMessage } from 'vfile-message';
+/* ·········································································· */
+import type { FileInstance } from './file';
+/* —————————————————————————————————————————————————————————————————————————— */
 
 export interface Content {
-  [key: string]: {
-    [key: string]: { [key: string]: unknown };
-  };
+  [entity: string]:
+    | {
+        [entry: string]:
+          | {
+              //
+              [property: string]: FileInstance | undefined;
+            }
+          | undefined;
+      }
+    | undefined;
 }
 
 export interface Schemas {
-  internals?: { [key: string]: JSONSchema7 };
-  content: { [key: string]: JSONSchema7 };
-  raw?: { [key: string]: string };
+  internals: { [schemaName: string]: JSONSchema7 };
+  content: { [schemaName: string]: JSONSchema7 };
+  raw: { [schemaName: string]: string };
+}
+
+export interface Errors {
+  [entity: string]:
+    | {
+        [entry: string]:
+          | {
+              [property: string]: PropertyReport | undefined;
+            }
+          | undefined;
+      }
+    | undefined;
 }
 
 export interface Types {
@@ -20,45 +49,66 @@ export interface Types {
   ide: string;
 }
 
-export interface ErrorsSchema {
-  [key: string]: unknown;
-  position?: Position;
+export interface FootnotesDefinition extends Partial<MdAstFootnoteDefinition> {
+  // label: string;
+  html: string;
+  // position?: UnistPosition;
+  // id: string;
 }
-export interface ErrorsLint {
-  [key: string]: unknown;
-  position?: Position;
+export interface FootnotesReference extends Partial<MdAstFootnoteReference> {
+  // label: string;
+  // id: string;
+  // html: string;
+  // position?: UnistPosition;
 }
-export interface ErrorsProse {
-  [key: string]: unknown;
-  position?: Position;
+export interface ReportFootnote {
+  references: FootnotesReference[];
+  definitions: FootnotesDefinition[];
 }
 
-export interface PropertyErrors {
-  schema: ErrorsSchema[];
-  lint: ErrorsLint[];
-  prose: ErrorsProse[];
+// export interface ReportFootnote {
+//   // [key: string]: unknown;
+//   position?: UnistPosition;
+// }
+export interface ReportLink extends Partial<Link> {
+  html: string;
 }
-export interface Errors {
-  [key: string]: {
-    [key: string]: {
-      [key: string]: PropertyErrors;
-    };
-  };
+export interface ErrorSchema extends AjvErrorObject {
+  // [key: string]: unknown;
+  position?: UnistPosition;
+}
+export interface ErrorLint extends VFileMessage {}
+export interface ErrorProse extends VFileMessage {}
+
+export type Reports =
+  | ErrorSchema[]
+  | ErrorLint[]
+  | ErrorProse[]
+  | FootnotesReference[]
+  | FootnotesDefinition[]
+  | (FootnotesReference | FootnotesDefinition)[]
+  | ReportLink[];
+
+export interface PropertyReport {
+  schema?: ErrorSchema[];
+  lint?: ErrorLint[];
+  prose?: ErrorProse[];
+
+  footnotes?: ReportFootnote;
+  links?: ReportLink[];
 }
 
 export interface ServerState {
-  content: Content | null;
+  content: Content;
 
-  schemas: Schemas | null;
+  schemas: Schemas;
 
-  errors: Errors | null;
+  errors: Errors;
 
-  types: Types | null;
+  types: Types;
 
-  config:
-    | {
-        previewUrl?: string;
-      }
-    | {}
-    | null;
+  config: {
+    previewUrl?: string;
+  };
 }
+export type Endpoint = keyof ServerState;
