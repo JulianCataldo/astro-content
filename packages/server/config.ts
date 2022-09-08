255571c4d2e7447178713bd79997c7a33109e150
Julian Cataldo <contact@juliancataldo.com>
2022-09-08 12:40:41 +0200

    refactor: disable config initializer
    Note: a dedicated default / global / user
    settings merger might not be needed for now

--- packages/server/config.ts
+++ packages/server/config.ts
@@ -1,65 +1,13 @@
 /* ·········································································· */
-import type { UserConfig } from '@astro-content/types/user-config';
+// import type { UserConfig } from '@astro-content/types/user-config';
 /* —————————————————————————————————————————————————————————————————————————— */
 
-let userConfig: Partial<UserConfig>;
+// let userConfig: Partial<UserConfig>;
 
-const ccompPath = './.ccomp';
+// const conf: UserConfig = {
+//   get previewUrl() {
+//     return userConfig?.previewUrl || '/';
+//   },
+// };
 
-const conf: UserConfig = {
-  get server() {
-    return {
-      host: userConfig?.server?.host || 'localhost',
-      port: userConfig?.server?.port || 5010,
-    };
-  },
-  get helpers() {
-    return { dest: userConfig?.helpers?.dest || `${ccompPath}/helpers` };
-  },
-  get components() {
-    return {
-      src: userConfig?.components?.src || './src/content',
-      dest: userConfig?.components?.dest || `${ccompPath}/build/content`,
-    };
-  },
-  get errors() {
-    return { dest: userConfig?.errors?.dest || './errors' };
-  },
-  get dev() {
-    return {
-      triggerFile:
-        userConfig?.dev?.triggerFile || `${ccompPath}/.timestamp.json`,
-    };
-  },
-  get vscode() {
-    return { dest: userConfig?.vscode?.dest || `${ccompPath}/schemas/vscode` };
-  },
-  get types() {
-    return { dest: userConfig?.types?.dest || `${ccompPath}/types` };
-  },
-  get markdown() {
-    return {
-      remarkPlugins: userConfig?.markdown?.remarkPlugins || [],
-    };
-  },
-  get fake() {
-    return {
-      entriesCount: userConfig?.fake?.entriesCount || 1200,
-    };
-  },
-  get remote() {
-    return {
-      dest: userConfig?.remote?.dest || null,
-    };
-  },
-  get log() {
-    return {
-      verbose: userConfig?.log?.verbose || true,
-    };
-  },
-  get previewUrl() {
-    return userConfig?.previewUrl || '/';
-  },
-};
-
-export { conf };
+// export { conf };
