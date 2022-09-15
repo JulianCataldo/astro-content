9d80a407e87a3ded1125fb864587ea85f9c27475
Julian Cataldo <contact@juliancataldo.com>
2022-09-15 02:18:02 +0200

    docs: default user settings

--- packages/types/user-config.ts
+++ packages/types/user-config.ts
@@ -1,6 +1,10 @@
 import type { LogLevel } from './logger';
 
 export interface UserConfig {
+  /** **Default**: `/` */
   previewUrl?: string;
+  /** **Default**: `'info'` */
   logLevel?: LogLevel;
+  /** **Default**: `true` */
+  gui?: boolean;
 }
