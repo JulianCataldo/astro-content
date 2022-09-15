53538be3d683af08490280c4f77a58aaca4a102e
Julian Cataldo <contact@juliancataldo.com>
2022-09-15 21:11:06 +0200

    refactor: ts to mjs

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -1,24 +1,20 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'astro/config';
 /* ·········································································· */
-import react from '@astrojs/react';
-import mdx from '@astrojs/mdx';
 import content from 'astro-content';
-
 /* —————————————————————————————————————————————————————————————————————————— */
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   server: {
-    port: 9054,
+    port: 3000,
     host: true,
   },
   integrations: [
-    // v————— Base: CLI + Server
-    content(),
-    // v————— (Optional) For GUI
-    react(),
-    mdx(),
+    content({
+      // logLevel: "debug",
+      // gui: true
+      // previewUrl: '/'
+    }),
   ],
 });
