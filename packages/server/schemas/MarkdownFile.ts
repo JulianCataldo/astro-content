36a06cb8fcd4187afbc1ac658d6e31d16840abd9
Julian Cataldo <contact@juliancataldo.com>
2022-09-28 02:11:27 +0200

    feat: enhance markdown internal schema

--- packages/server/schemas/MarkdownFile.ts
+++ packages/server/schemas/MarkdownFile.ts
@@ -1,16 +1,29 @@
+import type { JSONSchema7 } from 'json-schema';
+
 export default {
   title: 'Markdown file',
   description: 'Markdown content of this entry with frontmatter',
   properties: {
-    headings: {
+    file: { type: 'string' },
+    headingsCompiled: {
+      type: 'array',
       items: [
         {
-          properties: { level: { type: 'number' } },
-          required: ['level'],
+          properties: {
+            depth: { type: 'number' },
+            slug: { type: 'string' },
+            text: { type: 'string' },
+          },
+          required: ['depth', 'slug', 'text'],
+          additionalProperties: false,
         },
       ],
     },
-    Content: { type: 'object' },
+    // headings: {
+    //   type: 'null',
+    // },
+    Content: { type: 'string' },
   },
-  required: ['body', 'headings'],
-};
+  required: ['headings', 'Content'],
+  additionalProperties: false,
+} as JSONSchema7;
