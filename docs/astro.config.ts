1873342be6278eba66e09c34bdd4a2d60846b5fc
Julian Cataldo <contact@juliancataldo.com>
2022-10-07 09:23:22 +0200

    chore: update / remove / add dependencies

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -2,26 +2,25 @@
 import { defineConfig } from 'astro/config';
 /* ·········································································· */
 // import react from '@astrojs/react';
-import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+// import mdx from '@astrojs/mdx';
 import image from '@astrojs/image';
 import content from 'astro-content';
 import mdxMermaidPlugin from '@julian_cataldo/astro-diagram';
 /* —————————————————————————————————————————————————————————————————————————— */
+
 import remarkEmbed, {
   type Settings as RemarkEmbedSettings,
 } from 'remark-embed';
-import remarkGfm from 'remark-gfm';
-// import remarkMermaid from 'remark-mermaidjs';
-
+import remarkGfm from 'remark-gfm'; // import remarkMermaid from 'remark-mermaidjs';
 // FIXME: Sometimes, everything goes havoc during heavy dev. of AC sources
 // Here is a reminder: don't fiddle, just nuke the docs node_modules.
-
 // FIXME: MDX errors, for now, are all silent. Bad thing.
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-content.netlify.app',
-
+  site: 'https://astro-content.dev',
   server: {
     port: 9054,
     host: true,
@@ -32,29 +31,32 @@ export default defineConfig({
     // mdx(),
     content({
       //
-      logLevel: 'debug',
-      // gui: false,
+      logLevel: 'debug', // gui: false,
+    }), // ——————————————————————————————————————
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
     }),
-    // ——————————————————————————————————————
-    image(),
     // TODO: 1. Try mermaid with MDX
     // TODO: 2. Try embedding it in Astro Content / Or let user control this?
     // TODO: 3. Monitor any conflict when user has its own pipeline (merge?)
     // mdx({ remarkPlugins: [mdxMermaidPlugin] }),
+    sitemap(),
   ],
-
   // TODO: Try to embed it by default with Astro Content
   markdown: {
     remarkPlugins: [
       // () => () => {},
-      [remarkEmbed, { logLevel: 'info' } as RemarkEmbedSettings],
+      [
+        remarkEmbed,
+        {
+          logLevel: 'info',
+        } as RemarkEmbedSettings,
+      ],
       // [remarkMermaid, { launchOptions: { args: ['--no-sandbox'] } }],
       mdxMermaidPlugin,
       remarkGfm,
     ],
-  },
-
-  // vite: {
+  }, // vite: {
   //   css: {
   //     preprocessorOptions: {
   //       scss: {
