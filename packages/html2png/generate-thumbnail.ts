6b954a87c55f9d3083a13421c82a7818ca7028de
Julian Cataldo <contact@juliancataldo.com>
2023-01-23 20:52:21 +0100

    chore: init html2png package

---
+++ packages/html2png/generate-thumbnail.ts
@@ -0,0 +1,88 @@
+import { Resvg, initWasm } from '@resvg/resvg-wasm';
+// import fs from 'node:fs/promises';
+// import { Resvg } from '@resvg/resvg-js';
+
+import satori, { SatoriOptions } from 'satori';
+import type { html } from 'satori-html';
+/* ========================================================================== */
+
+// FIXME: Top level await bug
+let inited = false;
+async function init() {
+	await initWasm(fetch('https://unpkg.com/@resvg/resvg-wasm/index_bg.wasm'));
+	inited = true;
+}
+export function toBase64URL(buffer: Uint8Array) {
+	const url = URL.createObjectURL(new Blob([buffer], { type: 'image/png' }));
+	return url;
+}
+
+export async function html2png({
+	markup,
+	fonts,
+	width = 1200,
+	height = 630,
+	objectURL = false,
+}: {
+	markup: ReturnType<typeof html>;
+	fonts?: SatoriOptions['fonts'];
+	width?: number;
+	height?: number;
+	objectURL?: boolean;
+}) {
+	if (!inited) await init();
+
+	const m = markup as React.ReactNode; // Why assert?
+	const svg = await satori(m, {
+		width,
+		height,
+
+		fonts: fonts ?? [
+			{
+				name: 'Roboto',
+				data:
+					// await fs.readFile('./SourceSans3-Regular.otf.woff'),
+					await fetch(
+						'http://localhost:3000/SourceSans3-Regular.otf.woff',
+					).then((r) => r.arrayBuffer()),
+				weight: 400,
+				style: 'normal',
+			},
+		],
+	});
+
+	const opts = {
+		// ...
+	};
+	const resvg = new Resvg(svg, opts);
+	const pngData = resvg.render();
+	const pngBuffer = pngData.asPng();
+
+	return {
+		png: pngBuffer,
+		svg,
+
+		url: objectURL ? toBase64URL(pngBuffer) : undefined,
+
+		responses: {
+			png: [
+				pngBuffer,
+				{
+					status: 200,
+					headers: {
+						'Content-Type': 'image/png',
+					},
+				},
+			] as const,
+			svg: [
+				svg,
+				{
+					status: 200,
+					headers: {
+						'Content-Type': 'image/svg',
+					},
+				},
+			] as const,
+		},
+	};
+}
---
+++ packages/html2png/index.ts
@@ -0,0 +1,8 @@
+import { html2png, toBase64URL } from './generate-thumbnail.js';
+
+export { html2png, toBase64URL };
+
+export { html } from 'satori-html';
+export { css } from 'lit';
+
+export { styleMap } from 'lit/directives/style-map.js';
